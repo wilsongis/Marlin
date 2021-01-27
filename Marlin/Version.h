424665a97e9317ee90a57194bf2530436f0ed5e7
Mike Wilson <wilsongis@gmail.com>
2021-01-27 17:09:45 -0600

    Updated versiuon

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -46,7 +46,7 @@
 /**
  * Defines a generic printer name to be output to the LCD after booting Marlin.
  */
-#define MACHINE_NAME "Ender 3 Pro - v2.0.0"
+#define MACHINE_NAME "Ender 3 Pro - v2.0.1"
 
 /**
  * The SOURCE_CODE_URL is the location where users will find the Marlin Source

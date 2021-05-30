e831d50108341a40a270c3fb4928e866372aa44b
Mike Wilson <wilsongis@gmail.com>
2021-05-30 13:51:40 -0500

    Updated HR to 5

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -46,7 +46,7 @@
 /**
  * Defines a generic printer name to be output to the LCD after booting Marlin.
  */
-#define MACHINE_NAME "SWX1 v2.0.10" // Latest Marlin Updates
+#define MACHINE_NAME "SWX1 v2.0.11" // Latest Marlin Updates
 
 /**
  * The SOURCE_CODE_URL is the location where users will find the Marlin Source
